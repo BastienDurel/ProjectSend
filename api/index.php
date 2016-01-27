1303817c5863ef15153056dc3dc58d14b59eb799
Bastien Durel <bastien.durel@data.fr>
2016-01-27 11:33:18 +0100

    user: create

--- api/index.php
+++ api/index.php
@@ -13,6 +13,7 @@ getRoute()->get('/', 'showEndpoints');
 getApi()->get('users', array('Users', 'get_list'), EpiApi::external);
 getApi()->get('user/list', array('Users', 'get_list'), EpiApi::external);
 getApi()->get('user/(\d+)', array('Users', 'get'), EpiApi::external);
+getApi()->put('user', array('Users', 'create'), EpiApi::external);
 // TODO
 
 getRoute()->run();
@@ -26,6 +27,19 @@ function showEndpoints()
           <li>/user/(id) -> Get user by id</li>
           <!-- TODO -->
         </ul>';
+}
+
+function get_data() {
+    switch ($_SERVER['CONTENT_TYPE']) {
+    case 'application/x-www-form-urlencoded':
+        parse_str(file_get_contents("php://input"), $vars);
+        break;
+    default:
+        header('HTTP/1.1 500 Internal Server Error');
+        echo 'Unknown content-type';
+        exit();
+    }
+    return $vars;
 }
 
 class Users {
@@ -81,5 +95,47 @@ class Users {
         else
             return array();
         
+    }
+
+    function create() {
+        $vars = get_data();
+        error_log(print_r($vars, true));
+
+        global $database;
+        $database->MySQLDB();
+
+        // cleanup
+        $add_user_data_name = encode_html($vars['name']);
+        $add_user_data_email = encode_html($vars['email']);
+        $add_user_data_level = encode_html($vars['level']);
+        $add_user_data_user = encode_html($vars['user']);
+        $add_user_data_active = (isset($vars["active"])) ? 1 : 0;
+
+
+        $new_user = new UserActions();
+        $new_arguments = array(
+            'id' => '',
+            'username' => $add_user_data_user,
+            'password' => $vars['pass'],
+            'name' => $add_user_data_name,
+            'email' => $add_user_data_email,
+            'role' => $add_user_data_level,
+            'active' => $add_user_data_active,
+            'type' => 'new_user'
+        );
+
+        /** Validate the information from the posted data. */
+        $new_validate = $new_user->validate_user($new_arguments);
+
+        /** Create the user if validation is correct. */
+        if ($new_validate == 1) {
+            $new_response = $new_user->create_user($new_arguments);
+            return $new_response;
+        }
+        else {
+            header('HTTP/1.1 400 Bad Request');
+            global $valid_me;
+            return $valid_me->error_msg;
+        }
     }
 }
